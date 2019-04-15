0ea2c46afc4c660368a1d77ff738137d67f7d33e
Emir <navruzov.e@mail.ru>
2019-04-16 01:16:54 +0300

    with client tests

--- routes.py
+++ routes.py
@@ -3,4 +3,4 @@ from views import index, search_people
 
 def setup_routes(app):
     app.router.add_get('/', index, name='index')
-    app.router.add_get('/v1/search', search_people, name='search')
+    app.router.add_get('/api/v1/search', search_people, name='search')
