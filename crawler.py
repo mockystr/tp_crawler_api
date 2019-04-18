626bcdee57f9903dc7c526bf401bb399aa96bfc4
Emir <navruzov.e@mail.ru>
2019-04-19 02:17:49 +0300

    strange error if create es object in inti

--- crawler.py
+++ crawler.py
@@ -5,35 +5,44 @@ from bs4 import BeautifulSoup
 from settings import RPS, START_URL
 from urllib.parse import urljoin, urlparse, urldefrag
 from pprint import pprint
+from aioelasticsearch import Elasticsearch
 
 
 class Crawler:
     def __init__(self, start_url, rps=10, max_count=1000):
         self.start_url = start_url
         self.domain = '{uri.scheme}://{uri.netloc}/'.format(uri=urlparse(self.start_url))
-        print('domain ', self.domain)
         self.rps = rps
         self.max_count = max_count
         self.links = [self.start_url]
+        self.es = Elasticsearch()
 
     async def worker(self):
-        # self.redis_conn = await aioredis.create_connection(('localhost', 6379))
-
         async with aiohttp.ClientSession() as session:
-            for link in self.links:
+            for i, link in enumerate(self.links):
                 print(link)
                 async with session.get(link) as resp:
                     new_links, soup = await self.get_links(await resp.text())
-                    # print(new_links)
+
                     for n in new_links:
                         if n not in self.links:
                             self.links.append(n)
-                    await asyncio.sleep(1 / self.rps)
+
+                    print(await self.es.ping())
+                    ret = await self.es.create(index='emirloh',
+                                               doc_type='tptest',
+                                               id=i,
+                                               body={'text': 'asdasdas', 'url': link},
+                                               timeout=None)
+                    assert ret['result'] == 'created'
+
+                await asyncio.sleep(1 / self.rps)
             pprint(self.links)
 
     @staticmethod
-    async def clean_text(soup):
+    def clean_text(soup):
         [script.extract() for script in soup(["script", "style"])]
+        # await asyncio.sleep(0)
         text = soup.get_text()
         lines = [line.strip() for line in text.splitlines()]
         chunks = [phrase.strip() for line in lines for phrase in line.split("  ")]
@@ -54,3 +63,30 @@ if __name__ == '__main__':
     #     asyncio.run(Crawler(start_url=START_URL, rps=RPS).get_links(file))
 
     asyncio.run(Crawler(start_url=START_URL, rps=RPS).worker())
+
+
+"""
+/Users/emirnavruzov/.local/share/virtualenvs/tp_asyncio-qsY2M8-g/bin/python3.7 /Users/emirnavruzov/Documents/technopark/tp_asyncio/shit/es.py
+Traceback (most recent call last):
+  File "/Users/emirnavruzov/Documents/technopark/tp_asyncio/shit/es.py", line 22, in <module>
+    asyncio.run(Tmp().go())
+  File "/anaconda3/lib/python3.7/asyncio/runners.py", line 43, in run
+    return loop.run_until_complete(main)
+  File "/anaconda3/lib/python3.7/asyncio/base_events.py", line 584, in run_until_complete
+    return future.result()
+  File "/Users/emirnavruzov/Documents/technopark/tp_asyncio/shit/es.py", line 16, in go
+    print(await self.es.search())
+  File "/Users/emirnavruzov/.local/share/virtualenvs/tp_asyncio-qsY2M8-g/lib/python3.7/site-packages/aioelasticsearch/transport.py", line 338, in perform_request
+    ignore=ignore, timeout=timeout, headers=headers,
+  File "/Users/emirnavruzov/.local/share/virtualenvs/tp_asyncio-qsY2M8-g/lib/python3.7/site-packages/aioelasticsearch/transport.py", line 258, in _perform_request
+    ignore=ignore, timeout=timeout, headers=headers,
+  File "/Users/emirnavruzov/.local/share/virtualenvs/tp_asyncio-qsY2M8-g/lib/python3.7/site-packages/aioelasticsearch/connection.py", line 103, in perform_request
+    timeout=timeout or self.timeout,
+  File "/Users/emirnavruzov/.local/share/virtualenvs/tp_asyncio-qsY2M8-g/lib/python3.7/site-packages/aiohttp/client.py", line 417, in _request
+    with timer:
+  File "/Users/emirnavruzov/.local/share/virtualenvs/tp_asyncio-qsY2M8-g/lib/python3.7/site-packages/aiohttp/helpers.py", line 568, in __enter__
+    raise RuntimeError('Timeout context manager should be used '
+RuntimeError: Timeout context manager should be used inside a task
+Unclosed client session
+client_session: <aiohttp.client.ClientSession object at 0x10bc058d0>
+"""
